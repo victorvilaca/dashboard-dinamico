cb826cf7f932fdd0243fc3697a845c9cfaeda240
Victor Vilaça <vilacavictor@gmail.com>
2018-04-12 21:03:16 -0300

    Construção inicial da página. Adição do component de Select Field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SelectField from 'material-ui/SelectField';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (event, index, value) => {
+        this.setState({
+            value,
+        });
+    };
+
+    getSelectWidget(){
+        const style = {
+            botao: {
+                margin: 12,
+            },
+            selectWidget: {
+                display: 'flex',
+                alignItems: 'center',
+                alignContent: 'center'
+            }
+        };
+        return(
+            <div style={style.selectWidget}>
+                <SelectField
+                    hintText="Selecione um widget"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                >
+                    <MenuItem value={null} primaryText={false} />
+                </SelectField>
+                <RaisedButton label="Adicionar" style={style.botao} primary={true}/>
+            </div>
+        );
+    }
+
   render() {
     return (
       <div className="App">
+          <MuiThemeProvider>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src='https://viridis.energy/sites/default/files/logoViridisWhite.png' className="App-logo" alt="logo" />
+          <h1 className="App-title">Dashboard Dinâmico</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+              {this.getSelectWidget()}
+          </MuiThemeProvider>
       </div>
     );
   }
